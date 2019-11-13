b83434fa6439c1d47d79a6c7a00313eb6f836df0
Zheyu Wu <zheyuwu@g.harvard.edu>
2019-11-13 16:51:58 -0500

    added airbnb logo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
     $('#fullPage').fullpage();
+    // var svg = d3.select("#logo-animation").append("svg")
+    //                             .attr("width", 500)
+    //                             .attr("height", 500);
+    // var myimage = svg.append('image')
+    //                 .attr('xlink:href', "img/airbnb-logo.png")
+    //                 .attr('width', 400)
+    //                 .attr('height', 400)
+    source = "img/airbnb-logo.png";
+    var img = d3.select("#logo-animation").append("img").attr("src", source).style("opacity", 0)
+    img.transition().duration(2000).ease(d3.easeLinear).style("opacity", 1)
+    // myimage.transition()
+    //        .duration(1000)
+    //        .style("opacity", 0.2);
+
 })
