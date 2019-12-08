cc4ac1b6692d906e36283e9b7a8211e7f25e0339
Zheyu Wu <zheyuwu@g.harvard.edu>
2019-12-07 21:17:22 -0500

    changed the color to white

--- js/guest-view/grid-map.vis.js
+++ js/guest-view/grid-map.vis.js
@@ -188,7 +188,7 @@ gridMapVis.prototype.renderGrid = function() {
                 return "black";
             }
             else {
-                return "rgba(245, 245, 245, 0.8)";
+                return "white";
             }
         })
         .attr("opacity", function(d) {
